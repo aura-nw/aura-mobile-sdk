c46efc168e90870097a46bf730eb122f49fda963
Phạm Anh Tuấn <121532867+realphamanhtuan@users.noreply.github.com>
2023-09-14 08:22:20 +0700

    Remove C98 external wallet because of dependency conflicts

--- unity/aura-sdk/Assets/aura_sdk/external_wallet/config/Constant.cs
+++
@@ -1,6 +0,0 @@
-namespace AuraSDK{
-    public partial class Constant{
-        public const string SOCKET_SERVER_URI = "https://socket.coin98.services/";
-        public const float SOCKET_CONNECTION_TIMEOUT = 20f; //in seconds
-    }
-}
